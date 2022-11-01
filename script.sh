12b434b44de79271a2a69138043bd1631c533ba5
ynejmi <na>
2022-11-01 03:59:42 +0000

    correct suckless paths

--- script.sh
+++ script.sh
@@ -18,38 +18,33 @@ sudo pacman -Syu --needed --noconfirm zsh git curl base-devel sed
 
 #sudo bash ~/home-dir/my-arch-linux-magic/arch_install.sh
 
-sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-xsetroot xorg-xbacklight xorg-xprop \
-     noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono ttf-joypixels \
-     sxiv \
-     man-db xwallpaper python-pywal unclutter xclip maim \
-     xdotool brightnessctl  \
-     ntfs-3g sxhkd zsh \
-     arc-gtk-theme rsync dash \
-     jq \
-     rsync \
-     zsh-syntax-highlighting zsh-autosuggestions libconfig \
-     bluez bluez-utils
+sudo pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-xsetroot xorg-xbacklight xorg-xprop \
+     noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono ttf-joypixels ttf-font-awesome \
+     sxiv man-db xwallpaper python-pywal unclutter xclip maim bluez bluez-utils \
+     xdotool brightnessctl ntfs-3g sxhkd zsh zsh-syntax-highlighting zsh-autosuggestions \
+     arc-gtk-theme rsync dash jq rsync libconfig \
+     
 
-rm /bin/sh
-ln -s dash /bin/sh
-echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo rm /bin/sh
+sudo ln -s dash /bin/sh
+sudo echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 
 #sudo pacman -Syu --needed --noconfirm firefox code thunar
 
 
 sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-sudo  pacman-key --lsign-key FBA220DFC880C036
-sudo  pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 # Yay setup
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -fsri --noconfirm --needed
 
-sudo pacman -Sy libxft-bgra-git
+sudo pacman -Syu libxft-bgra-git
 
 
 
@@ -65,11 +60,11 @@ sudo mv /etc/xorg.conf.d /etc/X11
 
 cp -r ~/home-dir/.local/src ~/.local/
 
-sudo make clean install -C ~/.local/src/st
-sudo make clean install -C ~/.local/src/dwm
-sudo make clean install -C ~/.local/src/dwmblocks
-sudo make clean install -C ~/.local/src/dmenu
-sudo make clean install -C ~/.local/src/pinentry-dmenu
+sudo make clean install -C ~/.local/src/suckless/st
+sudo make clean install -C ~/.local/src/suckless/dwm
+sudo make clean install -C ~/.local/src/suckless/dwmblocks
+sudo make clean install -C ~/.local/src/suckless/dmenu
+sudo make clean install -C ~/.local/src/suckless/pinentry-dmenu
 
 # .config
 cp -r ~/home-dir/.config/zsh ~/.config/
